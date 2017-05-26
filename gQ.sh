eeb93ebd1193d213e416aeb873d6779d2c20fb75
cyberw0lf <dubey.abhineet@gmail.com>
2017-05-26 01:18:56 -0700

    updated git quick

--- gQ.sh
+++ gQ.sh
@@ -2,7 +2,15 @@
 # Copyright (c) 2017 Abhineet Dubey. All rights reserved.
 # Use of this source code is governed by the GPLv3 license that can be
 # found in the LICENSE file.
+if [ $# -eq 0 ]; then
+    echo "No commit message provided, gitQuick exiting"
+    echo "USAGE: ./gQ \"your git commit message\""
+    exit 1
+fi
+
 git add .
-#curtime=$(date)
+echo "All files added"
 git commit -m "$1"
+echo "commiting with the message" $1
 git push -u origin master
+echo "push done"
